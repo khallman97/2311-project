91f19cbfeb96fc19eb0d4956543775fe376cb519
khallman97 <khallman@my.yorku.ca>
2018-01-15 12:08:04 -0500

    test

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -5,5 +5,8 @@ public class ToyAuthoring {
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
     	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	    VisualPlayer test = new VisualPlayer(2,1);
+    	    
+    	    
     }
 }
