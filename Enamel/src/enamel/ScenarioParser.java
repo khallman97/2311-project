c9ddf36b71d150bc8a53f2065a9bb7f15d79a74a
khallman97 <khallman@my.yorku.ca>
2018-01-22 12:18:53 -0500

    Added the method to select scenario using a file

--- Enamel/src/enamel/ScenarioParser.java
+++ Enamel/src/enamel/ScenarioParser.java
@@ -537,5 +537,22 @@ public class ScenarioParser {
 							+ " you are looking for a different directory?");
 		}
 	}
-
+	
+	public void setScenarioFileWithFile(File file) {
+		try {
+			
+			fileScanner = new Scanner(file);
+			setCellAndButton();
+			play();
+		} catch (Exception e) {
+			errorLog("Exception error: " + e.toString(),
+					"Expected the directory path of the scenario file to"
+							+ " a file exists in the project folder. \n Could not find directory to path: "
+							+ file + " \n Perhaps" + " you forgot to add the file to the directory or "
+							+ " you are looking for a different directory?");
+		}
+	}
 }
+
+
+
