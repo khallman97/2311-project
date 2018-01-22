d2ec4b942060fbcab9e753f2405347850b48d478
Gagenpreet <gagenpreetbenipal@hotmail.ca>
2018-01-22 12:41:52 -0500

    Lab2 update

--- Enamel/src/enamel/Authoring.java
+++ Enamel/src/enamel/Authoring.java
@@ -1,12 +1,17 @@
 package enamel;
 
 import java.awt.event.*;
+import java.io.File;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import javax.accessibility.*;
 
-public class Authoring extends JFrame{
+public class Authoring extends JFrame implements ActionListener{
 	JPanel Panel = new JPanel();
+	JButton openButton;
+	JFileChooser chooser;
+	JTextArea output;
 	public Authoring() {
 		UI();
 		buttons();
@@ -17,11 +22,17 @@ public class Authoring extends JFrame{
 		getContentPane().add(Panel);
 		setSize(1000, 600);
 		setLocationRelativeTo(null);
+		output = new JTextArea(15, 20);
+		output.setDragEnabled(true);
+		Panel.add(output);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	private void buttons() {
 		//TODO make buttons
 		setTitle("File Chooser app");
+		openButton = new JButton("Open");
+		openButton.addActionListener(this);
+		Panel.add(openButton);
 	}
 	private void menu() {
 		//TODO make menu
@@ -33,15 +44,23 @@ public class Authoring extends JFrame{
                 System.exit(0);
             }
         });
-
         file.add(menuItem);
         menuBar.add(file);
         setJMenuBar(menuBar);
 	}
 	private void fileChooser() {
-		JFileChooser chooser = new JFileChooser();
-		chooser.setBounds(0, 75, 500, 300);
-		Panel.add(chooser);
+		chooser = new JFileChooser();
+		//FileFilter filter = new FileFilter();
+		//chooser.setFileFilter(filter);
+	}
+	public void actionPerformed(ActionEvent e){
+		if (e.getSource() == openButton){
+			int rep = chooser.showOpenDialog(Panel);
+			if (rep == JFileChooser.APPROVE_OPTION){
+				File file = chooser.getSelectedFile();
+				output.setText("Opening "+file.getName());
+			}
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
