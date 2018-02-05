1c6615ca2a69d99cde57fbc077aa71b931630117
gnuch <wah0530@yahoo.ca>
2018-02-05 12:31:07 -0500

    I added icons and mnemonics to the drop down options

--- Enamel/src/enamel/Authoring.java
+++ Enamel/src/enamel/Authoring.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.accessibility.*;
 
+//
+
 public class Authoring extends JFrame implements ActionListener{
 	GroupLayout layout = new GroupLayout(getContentPane());
 	JFileChooser chooser;
